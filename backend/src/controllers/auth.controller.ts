1fdd91fcd7435d8610ae31492a444a1c1963fa41
squach28 <squach3443@gmail.com>
2024-12-18 00:41:03 -0800

    add helper functions to check if email is taken and to register user

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,14 +1,68 @@
 import { Request, Response } from "express";
-import { pool } from "../db";
+import { pool } from "../database/db";
+import { queries } from "../database/queries";
+import { AuthDetails } from "../types/AuthDetails";
 
 export const login = (req: Request, res: Response) => {
   res.status(503).json({ message: "Not implemented yet" });
 };
 
-export const signup = (req: Request, res: Response) => {
-  res.status(503).json({ message: "Not implemented yet" });
+export const signup = async (req: Request, res: Response) => {
+  if (req.body === undefined) {
+    res.status(400).json({ message: "Bad request" });
+    return;
+  }
+  const emailTaken = await isEmailTaken(req.body.email);
+
+  if (emailTaken) {
+    res.status(400).json({ message: "Email already taken" });
+    return;
+  }
+  try {
+    const authDetails: AuthDetails = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    registerUser(authDetails);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const forgotPassword = (req: Request, res: Response) => {
   res.status(503).json({ message: "Not implemented yet" });
 };
+
+const isEmailTaken = async (email: string) => {
+  try {
+    const result = await pool.query(queries.getUserByEmail, [email]);
+    if (result.rows.length > 0) {
+      return false;
+    }
+    return true;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+const registerUser = async (authDetails: AuthDetails) => {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    await client.query(queries.addUserToAuth, [
+      authDetails.email,
+      authDetails.password,
+    ]);
+    await client.query("COMMIT");
+  } catch (e) {
+    console.log(e);
+    await client.query("ROLLBACK");
+    return false;
+  } finally {
+    client.release();
+  }
+};
+
+const hashPassword = (password: string) => {};
---
+++ backend/src/database/queries.ts
@@ -0,0 +1,4 @@
+export const queries = {
+  getUserByEmail: "SELECT email FROM auth WHERE email = $1",
+  addUserToAuth: "INSERT INTO auth (email, password) VALUES ($1, $2)",
+};
