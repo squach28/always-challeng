ca43a62445b779da3f6e83aff0deb1d0fc7f60cb
squach28 <squach3443@gmail.com>
2024-12-17 16:32:45 -0800

    add health endpoint on /

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    healthy: true,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
