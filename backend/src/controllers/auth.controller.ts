78567fa4802c308b0ac3e4af13dfc5a617efacc5
squach28 <squach3443@gmail.com>
2024-12-17 16:50:53 -0800

    add routes + controller function stubs for login, signup, and forgotPassword

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export const login = (req: Request, res: Response) => {};
+
+export const signup = (req: Request, res: Response) => {};
+
+export const forgotPassword = (req: Request, res: Response) => {};
