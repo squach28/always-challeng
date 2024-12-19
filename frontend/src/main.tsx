b93d15fcc6f0952ab60a58608eb283cd94bdd6c0
squach28 <squach3443@gmail.com>
2024-12-18 17:00:51 -0800

    create AuthLayout with LoginForm and SignupForm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import AuthLayout from "./pages/auth/AuthLayout.tsx";
+import LoginForm from "./pages/auth/forms/LoginForm.tsx";
+import SignupForm from "./pages/auth/forms/SignupForm.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route index path="/" element={<App />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Route>
+      <Route index element={<App />} />
+
+      <Route path="*" />
     </Routes>
   </BrowserRouter>
 );
